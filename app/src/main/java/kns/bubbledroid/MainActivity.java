7a5920a44ecf57cbb3a7824534874b82fc41331e
Budder21 <david.l.smith166@gmail.com>
2017-11-25 20:30:54 -0500

    added sound, refactored draw method

--- app/src/main/java/kns/bubbledroid/MainActivity.java
+++ app/src/main/java/kns/bubbledroid/MainActivity.java
@@ -1,6 +1,7 @@
 package kns.bubbledroid;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,11 +10,17 @@ import android.widget.ImageButton;
 public class MainActivity extends AppCompatActivity {
 
     private ImageButton playButton, helpButton;
+    private static MediaPlayer backgroundMusic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(backgroundMusic == null){
+            backgroundMusic = MediaPlayer.create(this,R.raw.background);
+            backgroundMusic.setLooping(true);
+            backgroundMusic.start();
+        }
 
         playButton = (ImageButton)findViewById(R.id.playButton);
         playButton.setOnClickListener(e->{
